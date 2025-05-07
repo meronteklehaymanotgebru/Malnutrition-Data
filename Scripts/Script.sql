c8fef2a3f067f97795793d88cc2999f21e796ad1
Meron T. Gebru <meronteklehaymanotgebru@gmail.com>
2025-05-07 08:54:00 +0300

    Added a child table for malnutrition data purposes

---
+++ Scripts/Script.sql
@@ -0,0 +1,21 @@
+insert into malnutrition.child_details(child_id, child_name,gender,date_of_birth)
+values('c02','Meron Gebre','female','2020-04-03'),
+('c03','Hana Abebe','female','2019-04-13'),
+('c04','Yonas Abebe','male','2021-03-23'),
+('c05','Abel Yonas','male','2020-04-15'),
+('c06','Abebe Tekle','male','2018-02-20'),
+('c07','Hermela Kahsay','female','2020-03-14'),
+('c08','Fana Selam','female','2016-08-04'),
+('c09','Helen Kahsay','female','2018-03-22'),
+('c10','Trhas Belay','female','2019-09-10'),
+('c11','Meron Belay','male','2020-03-15'),
+('c12','Haftom Gebre','male','2017-02-22'),
+('c13','Kebede Haftay','male','2019-05-13'),
+('c14','Frezer Kebede','male','2018-02-21'),
+('c15','Tekle Halefom','male','2021-01-22');
+--create table malnutrition.child_details(
+--child_id VARCHAR(10) primary key,
+--child_name VARCHAR(80) not null,
+--gender VARCHAR(15) not null,
+--date_of_birth DATE not null
+--);
